eb5896ac60f8e56f2500bb1f7376f390277b62ef
Tim Parsons <tparsons@brightcove.com>
2015-10-01 10:57:20 -0400

    Remove some old apps from remove.sh script

--- remove.sh
+++ remove.sh
@@ -3,19 +3,8 @@
 # This script will remove various apps from a device
 
 # Apps to remove
-#APP_NAME[0]="com.bellmedia.canald"
-#APP_NAME[1]="com.bellmedia.canalvie"
-#APP_NAME[2]="com.bellmedia.ztele"
-#APP_NAME[3]="com.bellmedia.vrak"
-#APP_NAME[0]="org.metopera"
 #APP_NAME[0]="com.bcgs.commonfilms"
-#APP_NAME[0]="org.metopera.dev"
-#APP_NAME[1]="org.metopera"
 APP_NAME[0]="com.brightcove.multiactivitysample"
-#APP_NAME[1]="org.metopera.dev.qa"
-#APP_NAME[6]="com.brightcove.admob"
-#APP_NAME[11]="com.birdjunior.trakalarm"
-#APP_NAME[12]="net.ajplus.ajplus"
 NUMAPPS=${#APP_NAME[@]}
 
 # Include the shell script that contains the function to select a device
