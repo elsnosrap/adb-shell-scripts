5bfce95dfaab65761870ead4a03e93c94d63a1f3
Tim Parsons <tparsons@brightcove.com>
2015-10-01 10:56:15 -0400

    Update dump_alarm script to include device selection logic

--- dump_alarms.sh
+++ dump_alarms.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-adb shell dumpsys alarm > alarms.txt
+
+# This script will dump all activities running on a device
+
+# Include the shell script that contains the function to select a device
+source $SHELL_SCRIPTS_HOME/adbwrapper-func.sh
+
+# Get the device to use for this command
+selectDevice SELECTED_DEVICE
+
+# Make sure the user selected a device
+if [[ "$SELECTED_DEVICE" = "0" ]]; then
+  echo "Please select a valid device"
+  exit 0;
+fi
+
+# Dump all the alarms on the device
+adb -s $SELECTED_DEVICE shell dumpsys alarm
+
