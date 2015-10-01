cc29316d9a078147a4b9033fbc502d1cf9a7a7c3
Tim Parsons <tparsons@brightcove.com>
2015-10-01 10:56:38 -0400

    Verify user input in lc.sh script

--- lc.sh
+++ lc.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #  lc.sh - A simplified logcat script.
 
+# Include the shell script that contains the function to select a device
 source $HOME/dev/git/shell-scripts/adbwrapper-func.sh
 
 # Grab the device to use
 selectDevice SELECTED_DEVICE
+
+# Make sure the user selected a device
+if [[ "$SELECTED_DEVICE" = "0" ]]; then
+  echo "Please select a valid device"
+  exit 0;
+fi
 
 # Clear the log
 adb -s $SELECTED_DEVICE logcat -c
