f6365c75c601353e71cc57023edc045823487e95
Tim Parsons <tparsons@brightcove.com>
2015-10-01 10:31:38 -0400

    Add new script to take a screen shot of device

---
+++ adbscreencap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script takes a screen shot from the selected device.
+# If there are multiple devices / emulators, this script will prompt for which device to use.
+
+# Include the shell script that contains the function to select a device
+source $SHELL_SCRIPTS_HOME/adbwrapper-func.sh
+
+# Get the device to use for this command
+selectDevice SELECTED_DEVICE
+
+# Make sure the user selected a device
+if [[ "$SELECTED_DEVICE" = "0" ]]; then
+  echo "Please select a valid device"
+  exit 0;
+fi
+
+# Execute the requested command
+adb -s ${SELECTED_DEVICE} shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > ./screencap.png
